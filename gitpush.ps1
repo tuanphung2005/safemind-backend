55f6637b81169570c7ce79f81d03a419f9f06a86
PHUNG ANH TUAN <tuan29102005@gmail.com>
2026-05-04 13:59:51 +0700

    gitpush

---
+++ gitpush.ps1
@@ -0,0 +1,5 @@
+Write-Host "[PS] pushing to GitHub..." -ForegroundColor Yellow
+git push origin main
+Write-Host "[PS] pushing to Hugging Face..." -ForegroundColor Yellow
+git push alt main
+Write-Host "[PS] done!!!!!!!!!!!!!" -ForegroundColor Green
